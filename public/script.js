c9f205db8dfbaa729d693ac96ca682b9477b5b0c
ccpony86 <19461718@qq.com>
2014-01-30 16:28:51 +0800

    update run

--- public/script.js
+++ public/script.js
@@ -2,116 +2,114 @@ var serverUrl = "/";
 var localStream, room, recording;
 
 function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-      results = regex.exec(location.search);
-  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex
+			.exec(location.search);
+	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g,
+			" "));
 }
 
-function startRecording (){
-  if (room!=undefined){
-    if (!recording){
-      room.startRecording(localStream);
-      recording = true;
-    }else{
-      room.stopRecording(localStream);
-      recording = false;
-    }
-  }
-}
+window.onload = function() {
+	recording = false;
+	var screen = getParameterByName("screen");
 
-window.onload = function () {
-  recording = false;
-  var screen = getParameterByName("screen");
+	localStream = Erizo.Stream({
+		streamID : 'local_stream_0',
+		audio : true,
+		video : true,
+		data : false,
+		screen : screen,
+		videoSize : [ 640, 480, 640, 480 ]
+	});
+	var createToken = function(userName, role, callback) {
 
-  //localStream = Erizo.Stream({audio: true, video: true, data: true, screen: screen, videoSize: [640, 480, 640, 480]});
-  localStream = Erizo.Stream({streamID:'localid', audio: true, video: true, data: false, screen: screen, videoSize: [640, 480, 640, 480]});
-  var createToken = function(userName, role, callback) {
+		var req = new XMLHttpRequest();
+		var url = serverUrl + 'createToken/';
+		var body = {
+			username : userName,
+			role : role
+		};
 
-    var req = new XMLHttpRequest();
-    var url = serverUrl + 'createToken/';
-    var body = {username: userName, role: role};
+		req.onreadystatechange = function() {
+			if (req.readyState === 4) {
+				callback(req.responseText);
+			}
+		};
 
-    req.onreadystatechange = function () {
-      if (req.readyState === 4) {
-        callback(req.responseText);
-      }
-    };
+		req.open('POST', url, true);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.send(JSON.stringify(body));
+	};
 
-    req.open('POST', url, true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.send(JSON.stringify(body));
-  };
+	createToken("user", "presenter", function(response) {
+		var token = response;
+		console.log(token);
+		room = Erizo.Room({
+			token : token
+		});
 
-  createToken("user", "presenter", function (response) {
-    var token = response;
-    console.log(token);
-    room = Erizo.Room({token: token});
+		localStream.addEventListener("access-accepted", function() {
+			console.log("my access-accepted");
+			var subscribeToStreams = function(streams) {
+				for ( var index in streams) {
+					var stream = streams[index];
+					if (localStream.getID() !== stream.getID()) {
+						room.subscribe(stream);
+					}
+				}
+			};
 
-    localStream.addEventListener("access-accepted", function () {
-	console.log("my access-accepted");
-      var subscribeToStreams = function (streams) {
-        for (var index in streams) {
-          var stream = streams[index];
-          if (localStream.getID() !== stream.getID()) {
-            room.subscribe(stream);
-          }
-        }
-      };
+			room.addEventListener("room-connected", function(roomEvent) {
 
-      room.addEventListener("room-connected", function (roomEvent) {
+				room.publish(localStream, {
+					maxVideoBW : 300
+				});
 
-        room.publish(localStream, {maxVideoBW: 300});
+				localStream.show("local_view_0");
+				
+				subscribeToStreams(roomEvent.streams);
+			});
 
-        subscribeToStreams(roomEvent.streams);
+			room.addEventListener("stream-subscribed", function(streamEvent) {
+				var stream = streamEvent.stream;
+				
+				var param = {
+					width : 320,
+					height : 240,
+					view_id : "remote_view_" + stream.getID()
+				};
+				pcManagerJS.call_method('view_new', "0", param);
+				stream.show("remote_view_" + stream.getID());
 
-//console.log("localStream.show");		
-//		localStream.show("myVideo");
-      });
+			});
 
-      room.addEventListener("stream-subscribed", function(streamEvent) {
-        var stream = streamEvent.stream;
-        /*var div = document.createElement('div');
-        div.setAttribute("style", "width: 320px; height: 240px;");
-        div.setAttribute("id", "test" + stream.getID());
+			room.addEventListener("stream-added", function(streamEvent) {
+				var streams = [];
+				streams.push(streamEvent.stream);
+				subscribeToStreams(streams);
+			});
 
-        document.body.appendChild(div);*/
-        var param = {width:320,height:240,view_id:"test" + stream.getID()};
-        pcManagerJS.call_method('view_new', "0", param);
-        stream.show("test" + stream.getID());
+			room.addEventListener("stream-removed", function(streamEvent) {
+				var stream = streamEvent.stream;
+				if (stream.elementID !== undefined) {
+					var param = {
+						view_id : "remote_view_" + stream.getID()
+					};
+					pcManagerJS.call_method('view_delete', "0", param);
+				}
+			});
 
-      });
+			console.log("room.connect");
+			room.connect();
+		});
 
-      room.addEventListener("stream-added", function (streamEvent) {
-        var streams = [];
-        streams.push(streamEvent.stream);
-        subscribeToStreams(streams);
-      });
+		var param = {
+			width : 320,
+			height : 240,
+			view_id : "local_view_0"
+		};
+		pcManagerJS.call_method('view_new', "0", param);
 
-      room.addEventListener("stream-removed", function (streamEvent) {
-        // Remove stream from DOM
-        var stream = streamEvent.stream;
-        if (stream.elementID !== undefined) {
-          //var element = document.getElementById(stream.elementID);
-          //document.body.removeChild(element);
-        	var param = {view_id:"test" + stream.getID()};
-        	pcManagerJS.call_method('view_delete', "0", param);
-        }
-      });
-
-      console.log("room.connect");
-      room.connect();
-	
-
-		
-
-    });
-
-var param = {width:320,height:240,view_id:"myVideo"};
-        pcManagerJS.call_method('view_new', "0", param);
-
-    localStream.init();
-	console.log("********************");
-
-  });
+		localStream.init();
+	});
 };
